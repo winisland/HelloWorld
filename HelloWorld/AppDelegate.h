9c8daf6b181e0591929b31d1667c070c7a6b5dd8
高島 勝利 <takashimakatsutoshi@takashima-no-MacBook-Air.local>
2012-11-03 16:52:34 +0900

    Initial Commit

---
+++ HelloWorld/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by 高島 勝利 on 2012/11/03.
+//  Copyright (c) 2012年 高島 勝利. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HelloWorld/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by 高島 勝利 on 2012/11/03.
+//  Copyright (c) 2012年 高島 勝利. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
